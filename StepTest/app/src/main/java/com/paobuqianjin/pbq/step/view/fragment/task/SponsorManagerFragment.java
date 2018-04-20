723afc8b473203ab17220eb9be4f59b132f30b71
paobuqianjing1 <34178973+paobuqianjing1@users.noreply.github.com>
2018-04-20 10:52:33 +0800

    commit
    commit

---
+++ StepTest/app/src/main/java/com/paobuqianjin/pbq/step/view/fragment/task/SponsorManagerFragment.java
@@ -0,0 +1,20 @@
+package com.paobuqianjin.pbq.step.view.fragment.task;
+
+import com.paobuqianjin.pbq.step.R;
+import com.paobuqianjin.pbq.step.view.base.fragment.BaseBarStyleTextViewFragment;
+
+/**
+ * Created by pbq on 2018/4/20.
+ */
+
+public class SponsorManagerFragment extends BaseBarStyleTextViewFragment {
+    @Override
+    protected int getLayoutResId() {
+        return R.layout.sponsor_mananger_fg;
+    }
+
+    @Override
+    protected String title() {
+        return "管理商铺信息";
+    }
+}
